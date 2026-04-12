756e2ce39dc5773dae5f553a66ab3e845ab98099
Yersai Mendez <yersai.mendez.dev@gmail.com>
2026-04-11 23:03:28 -0600

    Created the intial add and update to the project

--- ToDoAPP/MainWindow.xaml.cs
+++ ToDoAPP/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ToDoAPP
 {
@@ -16,9 +9,53 @@ namespace ToDoAPP
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int _editingIndex = -1;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            // 1. Get the text the user typed
+            string newTask = TaskInput.Text;
+
+            // 2. Make sure it's not empty before adding it
+            if (!string.IsNullOrWhiteSpace(newTask))
+            {
+                if (_editingIndex >= 0)
+                {
+                    // Update existing task
+                    TaskListView.Items[_editingIndex] = newTask;
+                    _editingIndex = -1;
+                    AddButton.Content = "Add Task";
+                }
+                else
+                {
+                    // 3. Add the task to the ListBox
+                    TaskListView.Items.Add(newTask);
+                }
+
+                // 4. Clear the text box so they can type a new task
+                TaskInput.Clear();
+            }
+        }
+
+        private void EditTask_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                var task = button.Tag as string;
+                _editingIndex = TaskListView.Items.IndexOf(task);
+
+                if (_editingIndex >= 0)
+                {
+                    TaskInput.Text = task;
+                    AddButton.Content = "Update Task";
+                }
+            }
         }
     }
 }
